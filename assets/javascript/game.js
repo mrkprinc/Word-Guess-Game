5ad4a273ab49b3827a7699e8fcb0501500eb76b7
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-06-03 22:22:42 -0400

    tentative final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,18 @@ var wordDiv = document.querySelector("#div-word");
 var guessedDiv = document.querySelector("#div-guessed");
 var winsCount = 0;
 var lossesCount = 0;
+var winSpan = document.querySelector("#span-wins");
+var loseSpan = document.querySelector("#span-losses");
+var manParts = ['hangman-legR', 'hangman-legL', 'hangman-armR', 'hangman-armL', 'hangman-body', 'hangman-head'];
 
 // game object
 
 var game = {
+    color: "",
     word: [],
     displayWord: [],
     guessed: [],
-    guessedWrong: ["a", "b", "c", "d"],
+    guessedWrong: [],
     lifeLeft: 5,
     gameWon: false,
     wrongGuess: true,
@@ -24,41 +28,64 @@ var game = {
         // wrongGuess starts true, changes to false if letter matches
 
         wrongGuess = true;
+        checkGuessed = this.guessed.indexOf(letterGuess);
+        if (checkGuessed == "-1") {
 
-        for(i = 0; i < this.word.length; i++) {
-            if(this.word[i] == letterGuess) {
-                this.displayWord[i] = this.word[i];
-                wrongGuess = false;
+            //code only runs if letter has not already been guessed
+            //find matching letters, change from "_" in the display word
+
+            for(i = 0; i < this.word.length; i++) {
+                if(this.word[i] == letterGuess) {
+                    this.displayWord[i] = this.word[i];
+                    wrongGuess = false;
+                }
             }
+    
+            if (wrongGuess) {
+                this.guessedWrong.push(letterGuess);
+    
+                // make next body part appear
+                var nextPart = document.getElementById(manParts[this.lifeLeft -1]);
+                nextPart.style.opacity = 1;
+    
+                this.lifeLeft -= 1;
+            }
+    
+            this.guessed.push(letterGuess);
+    
+            // if no guesses left, new round
+    
+            if (this.lifeLeft == 0) {
+                alert("You lose!");
+                lossesCount += 1;
+                newRound(chooseColor());
+    
+                //update score
+                loseSpan.innerHTML = lossesCount;
+            }
+    
+            // if word guessed, new round
+    
+            if (this.displayWord.indexOf("_") == "-1") {
+                alert("You win!");
+    
+                // change colors
+                document.getElementById("section-game").style.borderColor = this.color;
+                document.getElementById("section-hangman").style.borderColor = this.color;
+                document.getElementById("title").style.borderColor = this.color;
+                document.getElementById("score").style.borderColor = this.color;
+    
+                //update score
+                winsCount += 1;
+                winSpan.innerHTML = winsCount;
+
+                newRound(chooseColor());
+            }
+    
+            this.updateScreen();
         }
 
-        if (wrongGuess) {
-            this.guessedWrong.push(letterGuess);
-            this.lifeLeft -= 1;
-        }
-
-        console.log(this.lifeLeft);
-
-        this.guessed.push(letterGuess);
-
-        // if no guesses left, new round
-
-        if (this.lifeLeft == 0) {
-            alert("You lose!");
-            lossesCount += 1;
-            newRound("blue");
-        }
-
-        // if word guessed, new round
-
-        if (this.displayWord.indexOf("_") == "-1") {
-            alert("You win!");
-            winsCount += 1;
-            newRound("yellow");
-        }
-
-
-        this.updateScreen();
+        
     },
 
     // this function updates the screen after each user guess
@@ -92,8 +119,7 @@ var game = {
  
  // start a new round
 
- var wordIn = "orange";
- newRound(wordIn);
+ newRound(chooseColor());
 
 // listen for user pressing a letter
  
@@ -111,13 +137,21 @@ var game = {
 
     // reset values in game object
     
+    game.color = wordIn;
     game.word = [];
     game.displayWord = [];
     game.guessed = [];
     game.guessedWrong = [];
-    game.lifeLeft = 5;
+    game.lifeLeft = 6;
     game.gameWon = false;
     game.wrongGuess = true;
+
+    // reset hanging man
+
+    for(i=0; i < manParts.length; i++) {
+        var nextPart = document.getElementById(manParts[i]);
+        nextPart.style.opacity = "0";
+    }
 
     // fill arrays for word and displayWord
     
@@ -125,14 +159,18 @@ var game = {
         game.word.push(wordIn[i]);
     }
 
-    console.log(game.word);
-
     for(i=0; i < wordIn.length; i++) {
         game.displayWord.push("_");
     }
 
-    console.log(game.displayWord);
-
     game.updateScreen();
+ }
+
+ // this function chooses a random color from the word bank
+ 
+ function chooseColor() {
+    var wordBank = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+    var j = Math.floor(Math.random() * wordBank.length);
+    return wordBank[j];
  }
 
